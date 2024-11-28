498f683d4ec0816973004e439f73b131f6326d23
Christian Dräger <christian.draeger@t-systems.com>
2024-11-28 19:31:31 +0100

    chore: try to fix common dependencies

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -10,12 +10,26 @@ kotlin {
         browser()
         binaries.executable()
     }
+
+    // trying to fix it - Common.kt can find dependencies now
+    // getting exception when trying to run server:  Exception in thread "main" io.ktor.server.engine.internal.ReloadingException: Module function cannot be found for the fully qualified name 'ApplicationKt.module'
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlin.stdlib)
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.ktor.client.core)
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.kotlinx.rpc.core)
+        }
+    }
 }
 
-dependencies {
-    commonMainApi(libs.kotlin.stdlib)
-    commonMainApi(libs.kotlinx.serialization.json)
-    commonMainApi(libs.ktor.client.core)
-    commonMainApi(libs.kotlinx.coroutines.core)
-    commonMainApi(libs.kotlinx.rpc.core)
-}
+// taken from krpc docs but can not use dependencies than in commonMain
+
+//dependencies {
+//    commonMainApi(libs.kotlin.stdlib)
+//    commonMainApi(libs.kotlinx.serialization.json)
+//    commonMainApi(libs.ktor.client.core)
+//    commonMainApi(libs.kotlinx.coroutines.core)
+//    commonMainApi(libs.kotlinx.rpc.core)
+//}
